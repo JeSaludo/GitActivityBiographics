b4d08c2b8017868a63c4f6b85af5bc5e41874807
Jan <janerissaludo@gmail.com>
2022-03-14 11:37:43 +0800

    Information Added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+    strings:["Game Developer", "Student", "Programmer", "Software Developer", "Gamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+var typed = new Typed(".typing1", {
+    strings:["Game Developer", "Student", "Programmer", "Software Developer", "Gamer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
